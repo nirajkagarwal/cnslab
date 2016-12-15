ea6dda01e198de170385a93f27928651cb0bd881
Niraj Agarwal <niraj.k.agarwal93@gmail.com>
2016-12-15 23:45:06 +0530

    Edited Transposition

--- transposition.c
+++ transposition.c
@@ -1,72 +1,109 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+using namespace std;
 
 int main()
 {
-	int i,j,d,d1,k,l,r,len;
-	int seq[10];
-	char msg[100];
-	char plain[20][20], cipher[100];
-	printf("Enter the length of key sequence:\t");
-	scanf("%d",&l);
-	printf("Enter the key sequence:\n");
-	for(i=0;i<l;i++)
-		scanf("%d",&seq[i]);
-	getchar();
-	printf("Enter the message without spaces:\n");
-	scanf("%s",&msg);
-	len = strlen(msg);
-	r = len/l;
-	i = len-(l*r);
-	while(i!=0)
-	{
-		msg[len] = 'x';
-		len = len+1;
-		i--;
-	}
-	r=r+1;
-	k=0;
-	printf("The arrangement:\n");
-	for(i=0;i<r;i++)
-	{
-		for(j=0;j<l;j++)
-		{
-			plain[i][j] = msg[k++];
-			printf("%c ",plain[i][j]);
-		}
-		printf("\n");
-	}
-	k=0;
-	printf("\nEncryption\n");
-	for(i=0;i<l;i++)
-	{
-		for(j=0;j<r;j++)
-		{
-			cipher[k++] = plain[j][seq[i]-1];
-		}
-	}
-	printf("The arrangement:\n");
-	for(i=0;i<k;i++)
-	{
-		printf("%c",cipher[i]);
-	}
-	printf("\n");
-	k=0;
-	printf("\nDecryption\n");
-	for(i=0;i<l;i++)
-	{
-		for(j=0;j<r;j++)
-		{
-			plain[j][seq[i]-1] = cipher[k++];
-		}
-	}
-	printf("The arrangement:\n");
-	for(i=0;i<r;i++)
-	{
-		for(j=0;j<l;j++)
-		{
-			printf("%c",plain[i][j]);
-		}
-	}
+    cout<<"Enter the depth:\t";
+    int d;
+    cin>>d;
+    cout<<"Enter the length of key:\t";
+    int l1;
+    cin>>l1;
+    int seq[l1];
+    cout<<"Enter key sequence:\t";
+    for(int i=0;i<l1;i++)
+    {
+        cin>>seq[i];
+    }
+    cout<<"Enter string without spaces:\n";
+    char plain[100];
+    cin>>plain;
+    //Modifying Input
+    int len = strlen(plain);
+    if(len%l1!=0)
+    {
+        while(len%l1!=0)
+            plain[len++] = 'x';
+        plain[len] = '\0';
+        cout<<"Extra Char:\t"<<'x'<<endl;
+        cout<<"Modified plaintext:\t"<<plain<<endl;
+    }
+    int order[l1];
+    for(int i=1;i<=l1;i++)
+    {
+        for(int j=0;j<l1;j++)
+        {
+            if(seq[j]==i)
+                order[i-1] = j;
+        }
+    }
+    //Encryption
+    cout<<"\n\t\tENCRYPTION\n";
+    int d1 = d;
+    int r = len / l1;
+    char p[r][l1];
+    while(d1>0)
+    {
+        cout<<"\t\tTRANSPOSITION "<<(d-d1+1)<<endl;
+        int k=0;
+
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<l1;j++)
+            {
+                p[i][j]=plain[k++];
+            }
+        }
+        cout<<"The matrix arrangement is :\n";
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<l1;j++)
+                cout<<p[i][j]<<" ";
+            cout<<endl;
+        }
+        k=0;
+        for(int i=0;i<l1;i++)
+        {
+            for(int j=0;j<r;j++)
+            {
+                plain[k++] = p[j][order[i]];
+            }
+        }
+        cout<<"\nThe encrypted text is:\t"<<plain<<endl;
+        d1=d1-1;
+    }
+    //Decryption
+    cout<<"\n\t\tDECRYPTION\n";
+    while(d>0)
+    {
+        cout<<"\t\tTRANSPOSITION "<<d<<endl;
+        int k=0;
+        for(int i=0;i<l1;i++)
+        {
+            for(int j=0;j<r;j++)
+            {
+                p[j][order[i]] = plain[k++];
+            }
+        }
+        cout<<"The matrix arrangement is :\n";
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<l1;j++)
+                cout<<p[i][j]<<" ";
+            cout<<endl;
+        }
+        k=0;
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<l1;j++)
+            {
+                plain[k++]= p[i][j];
+            }
+        }
+        cout<<"\nThe decrypted text is:\t"<<plain<<endl;
+        d=d-1;
+    }
+    return 0;
 }
